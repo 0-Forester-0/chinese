3b3a5c4470da4fd18e509225126201d65e49434a
0-Forester-0 <dimasik98851@gmail.com>
2025-10-02 20:43:51 +0300

    Update forms.py

--- flashcards/forms.py
+++ flashcards/forms.py
@@ -38,30 +38,31 @@ class CardForm(forms.ModelForm):
         }
 
     def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
-    data = args[0] if args else None
-    category = None
-    if data:
-        category = data.get('category', None)
-    if not category and 'initial' in kwargs:
-        category = kwargs['initial'].get('category')
-    if category in ['HSK1', 'HSK2', 'HSK3']:
-        from .hsk_data import HSK1_CHARACTERS
-        from .hsk2_data import HSK2_CHARACTERS
-        from .hsk3_data import HSK3_CHARACTERS
-        hsk_map = {
-            'HSK1': HSK1_CHARACTERS,
-            'HSK2': HSK2_CHARACTERS,
-            'HSK3': HSK3_CHARACTERS,
-        }
-        chars = hsk_map[category]
-        self.fields['character'].choices = [(char, char) for char in chars]
-    else:
-        self.fields['character'].choices = []
+        super().__init__(*args, **kwargs)
+        data = args[0] if args else None
+        category = None
+        if data:
+            category = data.get('category', None)
+        if not category and 'initial' in kwargs:
+            category = kwargs['initial'].get('category')
+        if category in ['HSK1', 'HSK2', 'HSK3']:
+            from .hsk_data import HSK1_CHARACTERS
+            from .hsk2_data import HSK2_CHARACTERS
+            from .hsk3_data import HSK3_CHARACTERS
+            hsk_map = {
+                'HSK1': HSK1_CHARACTERS,
+                'HSK2': HSK2_CHARACTERS,
+                'HSK3': HSK3_CHARACTERS,
+            }
+            chars = hsk_map[category]
+            self.fields['character'].choices = [(char, char) for char in chars]
+        else:
+            self.fields['character'].choices = []
 
 class RegisterForm(UserCreationForm):
     class Meta:
         model = User
 
         fields = ['username', 'password1', 'password2']
+
 
